16c4ed00364a5b4c2cfbbc8d32beb38913faea2d
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-03-14 11:26:27 +0200

    first-app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,46 @@ const App = () => {
           </td>
           <td>
             <h4>Title</h4>
-            <p>Jottain tekstii</p>
+            <p>
+              Functions can be nested. Nested functions have access to variables
+              that were declared in the outer function. You can use nested
+              functions to organize the code in a function that’s long or
+              complex.
+            </p>
+          </td>
+          <td>
+            <a href="https://metropolia.fi">View</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="https://placekitten.com/181/100" alt="kissa" />
+          </td>
+          <td>
+            <h4>Title</h4>
+            <p>
+              Functions can be nested. Nested functions have access to variables
+              that were declared in the outer function. You can use nested
+              functions to organize the code in a function that’s long or
+              complex.
+            </p>
+          </td>
+          <td>
+            <a href="https://metropolia.fi">View</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="https://placekitten.com/182/100" alt="kissa" />
+          </td>
+          <td>
+            <h4>Title</h4>
+            <p>
+              Functions can be nested. Nested functions have access to variables
+              that were declared in the outer function. You can use nested
+              functions to organize the code in a function that’s long or
+              complex.
+            </p>
           </td>
           <td>
             <a href="https://metropolia.fi">View</a>
