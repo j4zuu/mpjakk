4d6098194ff5733699ff902063a731ac5fd0cee4
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-03-21 11:33:10 +0200

    osa-1

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
+
+const RegisterForm = (props) => {
+  return <div>RegisterForm</div>;
+};
+
+RegisterForm.propTypes = {};
+
+export default RegisterForm;
