a24e7de1efa9f759f9a14f50c26ef9fd46a3b56f
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-03-25 15:19:04 +0200

    profile-p1

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,19 @@
+import {useUser} from '../hooks/ApiHooks';
+
 const Profile = () => {
+  const {getUser} = useUser();
+  const fetchUser = async () => {
+    return await getUser(localStorage.getItem('token'));
+  };
+
+  const user = fetchUser();
+  console.log(user);
   return (
     <>
       <h1>Profile</h1>
+      <li>
+        <ul>{user.username}</ul>
+      </li>
     </>
   );
 };
